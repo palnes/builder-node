98f0bfb893b46daed46c4a353dd5c5eb6e75f61f
Pål Eivind <pal.nes@startsiden.no>
2013-08-16 19:30:10 +0200

    Added qunit task, uglifying jQuery

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,26 @@
+/*jshint indent:2 */
 
 
 module.exports = function(grunt) {
+  'use strict';
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    clean: ['node_modules', 'bower_components', 'static'],
-    uglify: {
+    clean: ['static'],
+    compass: {
       dist: {
-        files: {
-          'static/js/main.min.js': 'app/js/main.js'
+        options: {
+          config: 'config.rb'
         }
       }
     },
-    htmlmin: {
-        dist: {
 
-        } 
+    htmlmin: {
+      dist: {
+        files: {
+          'static/index.html': 'app/html/index.html' // 'destination': 'source'
+        }
+      }
     },
     jshint: {
       files: ['Gruntfile.js', 'app/js/**/*.js', 'test/**/*.js'],
@@ -30,33 +35,45 @@ module.exports = function(grunt) {
       }
     },
     bower: {
-        install: {
-            options: {
-                copy: false
-            }
+      install: {
+        options: {
+          copy: false
         }
+      }
+    },
+    qunit: {
+      all: ['test/**/*.html']
+    },
+    uglify: {
+      dist: {
+        files: {
+          'static/js/jquery.min.js': 'bower_components/jquery/jquery.js',
+          'static/js/main.min.js': 'app/js/main.js'
+        }
+      }
     },
     watch: {
       files: ['<%= jshint.files %>'],
-      tasks: ['jshint', 'qunit']
+      tasks: ['jshint']
     }
   });
 
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-compass');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-connect');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-htmlmin');
-    grunt.loadNpmTasks('grunt-contrib-imagemin');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-bower-task');
+  // Load grunt plugins/tasks
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-bower-task');
 
 
+  // Bundle tasks
   grunt.registerTask('test', ['jshint']);
-
-  grunt.registerTask('default', ['bower:install', 'jshint', 'uglify']);
-
+  grunt.registerTask('default', ['bower:install', 'jshint', 'uglify', 'compass', 'htmlmin']);
 };
