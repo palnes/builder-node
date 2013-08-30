a735823276a382bc1fd18acf55b296676741c03a
dagfinnr <dagfinn@startsiden.no>
2013-08-30 12:04:29 +0200

    BUILD-436 Just the bare minimum to make the repo run in builder with JS (I hope)

---
+++ t/basics.js
@@ -0,0 +1,8 @@
+// testing/nodeunit_basics.js
+module.exports = {
+    'Test 1' : function(test) {
+        test.expect(1);
+        test.ok(true, "This shouldn't fail");
+        test.done();
+    },
+};
