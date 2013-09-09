086e0f4b2ec5f432f6990874953dcb72febd9971
dagfinnr <dagfinn@startsiden.no>
2013-09-09 20:42:05 +0200

    BUILD-428 Slightly better test descriptions

--- t/basics.js
+++ t/basics.js
@@ -6,12 +6,12 @@ module.exports = {
         test.ok(fs.existsSync('./static/css/main.css'));
         test.done();
     },
-    'grunt generates index.html' : function(test) {
+    'grunt minifies HTML' : function(test) {
         test.expect(1);
         test.ok(fs.existsSync('./static/index.html'));
         test.done();
     },
-    'grunt minifies JavaScript' : function(test) {
+    'grunt uglifies JavaScript' : function(test) {
         test.expect(2);
         test.ok(fs.existsSync('./static/js/main.min.js'));
         test.ok(fs.existsSync('./static/js/jquery.min.js'));
