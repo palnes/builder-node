77f5936f8d6f464ceec74bee85730b8bfc8b9693
dagfinnr <dagfinn@startsiden.no>
2013-09-09 20:15:06 +0200

    BUILD-428 Splitting test for more readable test results

--- t/basics.js
+++ t/basics.js
@@ -1,10 +1,18 @@
 // testing/nodeunit_basics.js
 var fs = require('fs');
 module.exports = {
-    'grunt generates compiled files' : function(test) {
-        test.expect(4);
+    'grunt generates css' : function(test) {
+        test.expect(1);
         test.ok(fs.existsSync('./static/css/main.css'));
+        test.done();
+    },
+    'grunt generates index.html' : function(test) {
+        test.expect(1);
         test.ok(fs.existsSync('./static/index.html'));
+        test.done();
+    },
+    'grunt minifies JavaScript' : function(test) {
+        test.expect(2);
         test.ok(fs.existsSync('./static/js/main.min.js'));
         test.ok(fs.existsSync('./static/js/jquery.min.js'));
         test.done();
