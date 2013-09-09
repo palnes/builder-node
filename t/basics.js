b89ba9cc0929dd03e957ce114439e93bce7d5a64
dagfinnr <dagfinn@startsiden.no>
2013-09-09 13:39:52 +0200

    BUILD-428 Testing that grunt generates the correct files

--- t/basics.js
+++ t/basics.js
@@ -1,8 +1,12 @@
 // testing/nodeunit_basics.js
+var fs = require('fs');
 module.exports = {
-    'Test 1' : function(test) {
-        test.expect(1);
-        test.ok(true, "This shouldn't fail");
+    'grunt generates compiled files' : function(test) {
+        test.expect(4);
+        test.ok(fs.exists('./static/css/main.css'));
+        test.ok(fs.exists('./static/index.html'));
+        test.ok(fs.exists('./static/js/main.min.js'));
+        test.ok(fs.exists('./static/js/jquery.min.js'));
         test.done();
     },
 };
