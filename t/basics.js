adc0172bb78be1c2b488f3465defbe1801d10354
dagfinnr <dagfinn@startsiden.no>
2013-09-09 14:15:44 +0200

    BUILD-428 The method is called fs.existsSync() when used this way

--- t/basics.js
+++ t/basics.js
@@ -2,11 +2,11 @@
 var fs = require('fs');
 module.exports = {
     'grunt generates compiled files' : function(test) {
-        test.expect(4);
-        test.ok(fs.exists('./static/css/main.css'));
-        test.ok(fs.exists('./static/index.html'));
-        test.ok(fs.exists('./static/js/main.min.js'));
-        test.ok(fs.exists('./static/js/jquery.min.js'));
+        test.expect(1);
+        test.ok(fs.existsSync('./static/css/main.css'));
+        test.ok(fs.existsSync('./static/index.html'));
+        test.ok(fs.existsSync('./static/js/main.min.js'));
+        test.ok(fs.existsSync('./static/js/jquery.min.js'));
         test.done();
     },
 };
