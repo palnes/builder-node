af4885b6fbc7d1c604892b369b17b2fb64a01b44
dagfinnr <dagfinn@startsiden.no>
2013-10-29 11:50:31 +0100

    BUILD-441 Testing running both jasmine and perl

--- jasmine_plus_perl.pl
+++ jasmine_plus_perl.pl
@@ -1,6 +1,6 @@
 use lib '/build/lib';
 use Build::Debian::Test::Runner;
-my $runner = Build::Debian::Test::Runner->new(archive_file => 'archive.tgz');
+my $runner = Build::Debian::Test::Runner->new(archive_file => '/archive.tgz');
 my $r1 = Build::Debian::Test::Runner::Single::Jasmine->new(file => 'spec/hello.Spec.js');
 my $r2 = Build::Debian::Test::Runner::Single::Perl->new(file => 't/hello.t');
 $runner->add($r1, $r2);
